7c5ef2d7c16f75ae9e1ecdcaf436f237f9b6f601
Dan Foster <dan@zem.org.uk>
2014-01-29 13:44:26 +0000

    parse reads and writes seperate

--- io-profiler.py
+++ io-profiler.py
@@ -3,17 +3,22 @@
 import subprocess
 import os
 
-RRDTOOL='/usr/local/rrdtool-1.2.19/bin/rrdtool'
+RRDTOOL='rrdtool'
 sizes = {}
 
 def create_rrd(filename):
     output = [RRDTOOL,'create',filename]
     for i in range(5,22):
         bin_ = 2**i
-        o = "DS:"+str(bin_)+':GAUGE:600:U:U'
+        o = "DS:read-"+str(bin_)+':GAUGE:600:U:U'
+        output.append(o)
+        o = "DS:write-"+str(bin_)+':GAUGE:600:U:U'
         output.append(o)
     output.append('RRA:AVERAGE:0.5:1:24')
     output.append('RRA:AVERAGE:0.5:6:48')
+    output.append('RRA:AVERAGE:0.5:12:168')
+    output.append('RRA:AVERAGE:0.5:12:2016')
+    output.append('RRA:AVERAGE:0.5:288:365')
     subprocess.Popen(' '.join(output),shell=True, stdout=subprocess.PIPE)
 
 while True:
@@ -23,14 +28,15 @@ while True:
         disk = line.strip()
 
         if disk != "":
-	    disk = disk.replace('/','_')
+            [disk,type_] = disk.split()
+            disk = disk.replace('/','_').replace('?','Q')
             # Read and discard header line
             cmd.stdout.next()
 
             for line in cmd.stdout:
                 l = line.split()
                 if len(l) == 3:
-                    sizes[l[0]] = l[2]
+                    sizes[type_+"-"+l[0]] = l[2]
                 else:
                     break
             
